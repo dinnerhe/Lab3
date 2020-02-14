0a1189100aa8cd106cbebf01e53efc19beb8dc9c
Qiwei He <qiweih1@uci.edu>
2020-02-13 17:11:29 -0800

    Initial Commit

---
+++ Lab3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab3
+//
+//  Created by Tony on 2/13/20.
+//  Copyright © 2020 Tony. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
